91097acf2eb696acc3611bab55bd09a7fe2e65f8
Gordon Liew <gordonliewgl@gmail.com>
2021-04-22 23:10:38 +0800

    Added working app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from 'faker'; // used to generate fake images for avatars
+import CommentDetail from './CommentDetail';
+import ApprovalCard from "./ApprovalCard";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <h1>
+                    Warning!
+                </h1>
+                <div>
+                    Are you sure about that?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail 
+                    authorAvatar={faker.image.avatar()}
+                    author="Sam"
+                    timeAgo="Today at 1856hrs"
+                    commentText="Hello!"
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    authorAvatar={faker.image.avatar()}
+                    author="Gordon"
+                    timeAgo="Today at 2136hrs"
+                    commentText="Is it me?"
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail
+                    authorAvatar={faker.image.avatar()}
+                    author="Mike"
+                    timeAgo="Today at 2359hrs"
+                    commentText="You're looking for?"
+                />
+            </ApprovalCard>
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.querySelector('#root'));
